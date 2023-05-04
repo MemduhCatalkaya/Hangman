3e34db4db5fce708692b3b1abab8da155b4ba5a5
MemduhCatalkaya <dtmemduh@gmail.com>
2023-05-05 01:01:46 +0300

    TKinter imported and game changed completely

--- adamasmaca.py
+++ adamasmaca.py
@@ -1,37 +1,61 @@
+import tkinter as tk
 import random
-name = input("Enter your name:")
-print("welcome " + name + ", guess the Anime names by guessing letter by letter:")
-anime_list = ["death note", "naruto", "attack on titans", "no game no life", "sword art online", "one punch man", "deadman wonderland"]
-random_word = random.choice(anime_list)
-guess = "a"
+
+screen = tk.Tk()
+screen.title("Hangman")
+screen.minsize(width=600, height=300)
+
+game_frame = tk.Frame()
+game_frame.pack()
+
+button_frame = tk.Frame()
+button_frame.pack()
 lives = 10
-empty_guess_list = " "
-while lives > 0:
-	left = 0
-	for item in random_word:
-		if item in empty_guess_list:
-			print(item)
 
-		else:
-			print([])
-			left += 1
+anime_list = ["death note", "naruto", "attack on titans", "no game no life",
+              "sword art online", "one punch man", "deadman wonderland", "bleach",
+              "one piece", "fullmetal alchemist", "my hero academia", "jujutsu kaisen",
+              "code geass", "dragon ball"]
 
-	if left == 0:
-		print("You've Won!!")
-		break
+Letter_List = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+               "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
-	guess = input("Guess a letter:").lower()
-	if guess in "abcçdefgğhıijklmnoöprsştuüvyzqwx" and len(guess) != 0:
-	
-		empty_guess_list += guess
-	
-		if guess in random_word:
-			print("your guess is right!")
-		else:
-			lives -= 1
-			print("your guess is wrong!")
-			print("you have " + str(lives) + " lives left")
-			if lives == 0:
-				print("Sorry, you've lost")
-	else:
-		print("Please enter a letter...")
+random_anime = random.choice(anime_list)
+
+hidden_anime_list = [" " if letter == " " else "-" for letter in random_anime]
+hidden_anime = "".join(hidden_anime_list)
+
+
+def button_click(b):
+    global hidden_anime_list, hidden_anime, lives
+    if b in random_anime:
+        for ix, letter in enumerate(random_anime):
+            if letter == b:
+                hidden_anime_list[ix] = b
+        hidden_anime = "".join(hidden_anime_list)
+        anime_label.config(text=hidden_anime)
+    else:
+        lives -= 1
+        lives_label.config(text=f"You have {lives} lives!")
+    if lives == 0:
+        lives_label.config(text=f"You've Lost! The anime was '{random_anime}'")
+        for button in button_frame.winfo_children():
+            button["state"] = "disabled"
+    if "-" not in hidden_anime_list:
+        lives_label.config(text=f"You've WON!!")
+        for button in button_frame.winfo_children():
+            button["state"] = "disabled"
+
+
+anime_label = tk.Label(game_frame, text=hidden_anime, font=("Times", 20, "italic"))
+anime_label.pack()
+
+lives_label = tk.Label(game_frame, text=f"You have {lives} lives!", font=("Times", 20, "italic"))
+lives_label.pack()
+
+for i, letter in enumerate(Letter_List):
+    Buttons = tk.Button(button_frame, text=letter, width=5, height=2, command=lambda l=letter: button_click(l))
+    Buttons.grid(row=i // 6, column=i % 6)
+
+
+screen.mainloop()
