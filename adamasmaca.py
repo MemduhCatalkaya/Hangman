c8ee20c6c1e5a09e1cbc495fa745a7d22219c857
MemduhCatalkaya <dtmemduh@gmail.com>
2023-05-05 15:03:36 +0300

    cosmetic and functional changes

--- adamasmaca.py
+++ adamasmaca.py
@@ -1,16 +1,23 @@
 import tkinter as tk
 import random
 
+LIVES = 7
+BG = "#F8F6F0"
+
 screen = tk.Tk()
 screen.title("Hangman")
 screen.minsize(width=600, height=300)
+screen.configure(bg=BG)
 
-game_frame = tk.Frame()
+# frames
+
+game_frame = tk.Frame(bg=BG)
 game_frame.pack()
-
-button_frame = tk.Frame()
+button_frame = tk.Frame(bg=BG)
 button_frame.pack()
-lives = 10
+restart_and_hint_frame = tk.Frame(bg=BG)
+restart_and_hint_frame.pack()
+
 
 anime_list = ["death note", "naruto", "attack on titans", "no game no life",
               "sword art online", "one punch man", "deadman wonderland", "bleach",
@@ -22,12 +29,18 @@ Letter_List = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
 
 random_anime = random.choice(anime_list)
 
-hidden_anime_list = [" " if letter == " " else "-" for letter in random_anime]
+hidden_anime_list = ["  " if letter == " " else "□" for letter in random_anime]
 hidden_anime = "".join(hidden_anime_list)
 
 
 def button_click(b):
-    global hidden_anime_list, hidden_anime, lives
+    for button in button_frame.winfo_children():
+        if button["text"].lower() == b:
+            button["state"] = "disabled"
+            button["text"] = ""
+            button["bg"] = BG
+            button["borderwidth"] = 0
+    global hidden_anime_list, hidden_anime, LIVES
     if b in random_anime:
         for ix, letter in enumerate(random_anime):
             if letter == b:
@@ -35,27 +48,28 @@ def button_click(b):
         hidden_anime = "".join(hidden_anime_list)
         anime_label.config(text=hidden_anime)
     else:
-        lives -= 1
-        lives_label.config(text=f"You have {lives} lives!")
-    if lives == 0:
+        LIVES -= 1
+        lives_label.config(text=f"You have {LIVES} lives! Guess the anime name.")
+    if LIVES == 0:
         lives_label.config(text=f"You've Lost! The anime was '{random_anime}'")
         for button in button_frame.winfo_children():
             button["state"] = "disabled"
-    if "-" not in hidden_anime_list:
-        lives_label.config(text=f"You've WON!!")
+    if "□" not in hidden_anime_list:
+        lives_label.config(text=f"You've WON!! Your Score is {LIVES*1000}!")
         for button in button_frame.winfo_children():
             button["state"] = "disabled"
 
 
-anime_label = tk.Label(game_frame, text=hidden_anime, font=("Times", 20, "italic"))
-anime_label.pack()
-
-lives_label = tk.Label(game_frame, text=f"You have {lives} lives!", font=("Times", 20, "italic"))
+lives_label = tk.Label(game_frame, text=f"You have {LIVES} lives! Guess the anime name.",
+                       bg=BG, font=("Times", 20, "italic", "bold"))
 lives_label.pack()
 
-for i, letter in enumerate(Letter_List):
-    Buttons = tk.Button(button_frame, text=letter, width=5, height=2, command=lambda l=letter: button_click(l))
-    Buttons.grid(row=i // 6, column=i % 6)
+anime_label = tk.Label(game_frame, bg=BG, text=hidden_anime, font=("Times", 35, "italic"))
+anime_label.pack()
 
+for i, letter in enumerate(Letter_List):
+    Buttons = tk.Button(button_frame, text=letter.upper(), borderwidth=5, bg="#3B9C9C", width=5,
+                        height=2, command=lambda l=letter: button_click(l))
+    Buttons.grid(row=i // 6, column=i % 6, pady=2, padx=2)
 
 screen.mainloop()
