95943911e6eb7b79cb2576c3cacd4e4153e147e0
MemduhCatalkaya <dtmemduh@gmail.com>
2023-05-05 17:15:38 +0300

    restart and hint buttons added

--- adamasmaca.py
+++ adamasmaca.py
@@ -3,6 +3,7 @@ import random
 
 LIVES = 7
 BG = "#F8F6F0"
+BUTTON_BG = "#3B9C9C"
 
 screen = tk.Tk()
 screen.title("Hangman")
@@ -29,6 +30,12 @@ Letter_List = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
 
 random_anime = random.choice(anime_list)
 
+hint_set = set(["ç" if x == " " else x for x in random_anime])
+hint_set.add("ç")
+hint_set.remove("ç")
+hint_list = list(hint_set)
+
+
 hidden_anime_list = ["  " if letter == " " else "□" for letter in random_anime]
 hidden_anime = "".join(hidden_anime_list)
 
@@ -40,24 +47,55 @@ def button_click(b):
             button["text"] = ""
             button["bg"] = BG
             button["borderwidth"] = 0
-    global hidden_anime_list, hidden_anime, LIVES
+    global hidden_anime_list, hidden_anime, LIVES, hint_list
     if b in random_anime:
         for ix, letter in enumerate(random_anime):
             if letter == b:
                 hidden_anime_list[ix] = b
         hidden_anime = "".join(hidden_anime_list)
         anime_label.config(text=hidden_anime)
+        hint_list.remove(b)
     else:
         LIVES -= 1
         lives_label.config(text=f"You have {LIVES} lives! Guess the anime name.")
     if LIVES == 0:
         lives_label.config(text=f"You've Lost! The anime was '{random_anime}'")
+        hint_button["state"] = "disabled"
         for button in button_frame.winfo_children():
             button["state"] = "disabled"
     if "□" not in hidden_anime_list:
         lives_label.config(text=f"You've WON!! Your Score is {LIVES*1000}!")
+        hint_button["state"] = "disabled"
         for button in button_frame.winfo_children():
             button["state"] = "disabled"
+
+
+def restart_click():
+    global LIVES, random_anime, hidden_anime
+    global anime_label, lives_label, hidden_anime_list, hint_list, hint_set
+    LIVES = 7
+    global Buttons
+    random_anime = random.choice(anime_list)
+    hidden_anime_list = ["  " if letter == " " else "□" for letter in random_anime]
+    hidden_anime = "".join(hidden_anime_list)
+    hint_set = set(["ç" if x == " " else x for x in random_anime])
+    hint_set.add("ç")
+    hint_set.remove("ç")
+    hint_list = list(hint_set)
+    lives_label.config(text=f"You have {LIVES} lives! Guess the anime name.")
+    anime_label.config(text=hidden_anime)
+    hint_button["state"] = "normal"
+    for ix2, letter in enumerate(Letter_List):
+        Buttons = tk.Button(button_frame, text=letter.upper(), borderwidth=5, bg=BUTTON_BG, width=5,
+                            height=2, command=lambda l=letter: button_click(l))
+        Buttons.grid(row=ix2 // 6, column=ix2 % 6, pady=2, padx=2)
+
+
+def hint_click():
+    global hint_list
+    x = hint_list[random.randint(0, len(hint_list)-1)]
+    button_click(x)
+    hint_button["state"] = "disabled"
 
 
 lives_label = tk.Label(game_frame, text=f"You have {LIVES} lives! Guess the anime name.",
@@ -68,8 +106,17 @@ anime_label = tk.Label(game_frame, bg=BG, text=hidden_anime, font=("Times", 35, 
 anime_label.pack()
 
 for i, letter in enumerate(Letter_List):
-    Buttons = tk.Button(button_frame, text=letter.upper(), borderwidth=5, bg="#3B9C9C", width=5,
+    Buttons = tk.Button(button_frame, text=letter.upper(), borderwidth=5, bg=BUTTON_BG, width=5,
                         height=2, command=lambda l=letter: button_click(l))
     Buttons.grid(row=i // 6, column=i % 6, pady=2, padx=2)
+
+restart_button = tk.Button(restart_and_hint_frame, text="RESTART", borderwidth=8, bg=BUTTON_BG,
+                           width=10, height=2, command=restart_click)
+restart_button.grid(row=1, column=2, padx=2, pady=2)
+
+hint_button = tk.Button(restart_and_hint_frame, text="HINT", borderwidth=8, bg=BUTTON_BG,
+                        width=10, height=2, command=hint_click)
+hint_button.grid(row=1, column=1, padx=2, pady=2)
+
 
 screen.mainloop()
